d0859914b1cc552a49bbd5713e62e0c57a266238
DidacH <didac.hierro@gmail.com>
2024-10-12 12:19:30 +0200

    UCS implemented

--- search.py
+++ search.py
@@ -201,7 +201,37 @@ def breadth_first_search(problem):
 def uniform_cost_search(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    util.raise_not_defined()
+    pri_queue = util.PriorityQueue()
+    start_state = problem.get_start_state()
+    pri_queue.push((start_state, [], 0), 0)  #(state, path of actions to reach that state, cost to reach that state)
+
+    #Create a set (tuple) to keep track of the visited states
+    visited = set()
+
+    while not pri_queue.is_empty():
+        #Dequeue the item with the lowest cost path from the priority queue
+        current_state, actions, current_cost = pri_queue.pop()
+
+        #If this state has already been visited, skip it (whole iteration skipped)
+        if current_state in visited:
+            continue
+
+        #Mark the current state as visited
+        visited.add(current_state)
+
+        #If it's the goal state, return the actions (path) that got us here
+        if problem.is_goal_state(current_state):
+            return actions
+        
+        #Expand the current state to get its successors
+        for successor, action, step_cost in problem.get_successors(current_state):
+            if successor not in visited:
+                #Enqueue the successor onto the priority queue with the updated path of actions and the new cost
+                pri_queue.push((successor, actions + [action], current_cost + step_cost), current_cost + step_cost)
+
+    #If no solution is found, return an empty list
+    return []
+
 
 def null_heuristic(state, problem=None):
     """
